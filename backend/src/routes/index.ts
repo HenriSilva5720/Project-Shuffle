efaa110271876c58a1320aed81ce8779bf945c44
henrique <henrique.da.silva5720@gmail.com>
2022-03-25 13:49:19 -0300

    feat: errorHandler

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,17 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import userRoutes from "./user.routes";
+import { ErrorHandler, handleError } from "../utils/error";
 
 function routes(app: Express) {
   app.use(express.json());
 
   userRoutes(app);
+
+  app.use(
+    (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+      handleError(err, res);
+    }
+  );
 }
 
 export default routes;
